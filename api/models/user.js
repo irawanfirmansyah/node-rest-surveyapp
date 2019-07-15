5e8efb27ff06853df0eac08ff472fd3d944a5dce
Irawan Firmansyah <irawanfrmnsyh@gmail.com>
2019-07-15 17:59:32 +0700

    new user route, create and delete

--- api/models/user.js
+++ api/models/user.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose');
 
 const userSchema = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
-    name: { type: String, required: true },
+    fullName: { type: String, required: true },
+    username: { type: String, required: true },
     email: { type: String, required: true },
     password: { type: String, required: true }
 });
--- api/routes/user.js
+++ api/routes/user.js
@@ -1,24 +1,90 @@
 const express = require('express')
 const router = express.Router();
-const mongoose = reuire('mongoose');
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const User = require('../models/user');
 
 router.post('/login', (request, response, next) => {
     response.status(200).json({
-        message : 'Login success!!'
+        message: 'Login success!!'
     });
 });
 
 router.post('/signup', (request, response, next) => {
-    response.status(201).json({
-        message: 'Your account have been created!!'
-    });
+    User.findOne({
+        $or: [{ username: request.body.username }, { fullName: request.body.fullName }, { email: request.body.email }]
+    },
+        (err, result) => {
+            if (result) {
+                return response.status(409).json({
+                    message: `User with such username / name / email already exists`
+                });
+            }
+            else if (err) {
+                return response.status(500).json({
+                    error: err
+                });
+            }
+            else {
+                bcrypt.hash(request.body.password, 10, (err, hashedPassword) => {
+                    if (err) {
+                        return response.status(500).json({
+                            error: err
+                        });
+                    } else {
+                        const newUser = new User({
+                            _id: mongoose.Types.ObjectId(),
+                            fullName: request.body.fullName,
+                            username: request.body.username,
+                            email: request.body.email,
+                            password: hashedPassword
+                        });
+                        newUser.save()
+                            .then(result => {
+                                console.log(result);
+                                response.status(201).json({
+                                    message: `User created`,
+                                    body: result
+                                });
+                            })
+                            .catch(err => {
+                                console.log(err);
+                                response.status(500).json({
+                                    error: err
+                                });
+                            });
+                    }
+                });
+            }
+        });
 });
 
 router.delete('/:userId', (request, response, next) => {
-    response.status(201).json({
-        message: 'Delete account success',
-        id: request.params.userId
-    });
+    const id = request.params.userId;
+    User.findById(id)
+        .then(result => {
+            if (!result) {
+                response.status(404).json({
+                    error: `No user entry with such ID`
+                });
+            } else {
+                User.findById(request.body.id)
+                    .then(async (res) => {
+                        const match = await bcrypt.compare(request.body.password, res.password);
+                        if (match) {
+                            User.deleteOne({ _id: id })
+                                .then(() => {
+                                    response.status(200).json({ message: `User Account with ${id} ID has been deleted` });
+                                });
+                        }
+                        else {
+                            return response.status(500).json({
+                                error: `The password you've entered is incorrect. Try again`
+                            })
+                        }
+                    })
+            }
+        });
 });
 
 module.exports = router;
