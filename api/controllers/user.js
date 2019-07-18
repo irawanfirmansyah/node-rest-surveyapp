0ed473fc82d8d577b05d4a67ebab66566d17c9c7
Irawan Firmansyah <irawanfrmnsyh@gmail.com>
2019-07-18 17:33:52 +0700

    finish change user's syncroute into asyncroute

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -2,114 +2,87 @@ const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 const User = require('../models/user');
 const jwt = require('jsonwebtoken');
+const userHelper = require('../helpers/user.helper');
+const asyncMiddleware = require('../middleware/async-middleware');
 
-exports.user_create = (request, response, next) => {
-    User.findOne({
-        $or: [{ username: request.body.username }, { fullName: request.body.fullName }, { email: request.body.email }]
-    },
-        (err, result) => {
-            if (result) {
-                return response.status(409).json({
-                    message: `User with such username / name / email already exists`
-                });
-            }
-            else if (err) {
-                return response.status(500).json({
-                    error: err
-                });
-            }
-            else {
-                bcrypt.hash(request.body.password, 10, (err, hashedPassword) => {
-                    if (err) {
-                        return response.status(500).json({
-                            error: err
-                        });
-                    } else {
-                        const newUser = new User({
-                            _id: mongoose.Types.ObjectId(),
-                            fullName: request.body.fullName,
-                            username: request.body.username,
-                            email: request.body.email,
-                            password: hashedPassword
-                        });
-                        newUser.save()
-                            .then(result => {
-                                response.status(201).json({
-                                    message: `User created`,
-                                    body: result
-                                });
-                            })
-                            .catch(err => {
-                                return response.status(500).json({
-                                    error: err
-                                });
-                            });
-                    }
-                });
-            }
+exports.user_create = asyncMiddleware(async (request, response, next) => {
+    //Check user if username/email already used
+    const usernameIsUsed = await userHelper.getUserByUsernameOrEmail(request.body.username);
+    const emailIsUsed = await userHelper.getUserByUsernameOrEmail(request.body.email);
+    if (usernameIsUsed || emailIsUsed) {
+        const error = new Error('User with such username / name / email already exists');
+        error.status = 409;
+        throw error;
+    }
+    //Hashing password
+    const hash = await bcrypt.hash(request.body.password, 10);
+    if (!hash) {
+        throw new Error();
+    }
+    //Create new user
+    const newUser = new User({
+        _id: mongoose.Types.ObjectId(),
+        fullName: request.body.fullName,
+        username: request.body.username,
+        email: request.body.email,
+        password: hash
+    });
+    const result = await userHelper.create(newUser);
+    response.status(201).json({
+        message: `User created`,
+        body: {
+            _id: result.id,
+            fullName: result.fullName,
+            username: result.username,
+            email: result.email
+        }
+    });
+});
+
+exports.user_login = asyncMiddleware(async (req, res, next) => {
+    const result = await userHelper.getUserByUsernameOrEmail(req.body.emailOrUsername);
+    //Check user is exist
+    if (!result) {
+        const error = new Error('Login failed. The username or email you\'ve entered is not registered');
+        error.status = 401;
+        throw error;
+    }
+    const match = await bcrypt.compare(req.body.password, result.password);
+    if (!match) {
+        const error = new Error('Login failed. The password you\'ve entered is incorrect. Try again.')
+        error.status = 401;
+        throw error;
+    }
+    const token = jwt.sign(
+        {
+            email: result.email,
+            userId: result._id
+        },
+        process.env.JWT_KEY,
+        {
+            expiresIn: "1h"
         });
-}
+    res.status(200).json({
+        message: 'Authentication successful',
+        token: token
+    });
+});
 
-exports.user_login = (req, res, next) => {
-    User.findOne({ $or: [{ username: req.body.emailOrUsername }, { email: req.body.emailOrUsername }] })
-        .then(async (result) => {
-            const match = await bcrypt.compare(req.body.password, result.password);
-            if (match) {
-                const token = jwt.sign(
-                    {
-                        email: result.email,
-                        userId: result._id
-                    },
-                    process.env.JWT_KEY,
-                    {
-                        expiresIn: "1h"
-                    }
-                );
-                return res.status(200).json({
-                    message: 'Authentication successful',
-                    token: token
-                });
-            } else {
-                return res.status(401).json({
-                    message: 'Login failed. The password you\'ve entered is incorrect'
-                });
-            }
-        }).catch(() => {
-            return res.status(401).json({
-                message: 'Login failed. The username or email you\'ve entered is doesnt exist'
-            });
-        })
-}
-
-exports.user_delete = (request, response, next) => {
-    const id = request.params.userId;
-    User.findById(id)
-        .then(result => {
-            if (!result) {
-                return response.status(404).json({
-                    error: `No user entry with such ID`
-                });
-            } else {
-                User.findById(request.body.id)
-                    .then(async (res) => {
-                        const match = await bcrypt.compare(request.body.password, res.password);
-                        if (match) {
-                            User.deleteOne({ _id: id })
-                                .then(() => {
-                                    return response.status(200).json({ message: `User Account with ${id} ID has been deleted` });
-                                });
-                        }
-                        else {
-                            return response.status(401).json({
-                                error: `The password you've entered is incorrect. Try again`
-                            })
-                        }
-                    })
-                    .catch(() => {
-                        return response.status(500).json({
-                            message: 'Internal Error'
-                        })
-                    })
-            }
-        });
-}
+exports.user_delete = asyncMiddleware(async (request, response, next) => {
+    const userTargetId = request.params.userId;
+    const userTargetIsExist = await userHelper.getUserById(userTargetId);
+    if (!userTargetIsExist) {
+        const error = new Error('No user entry with such ID');
+        error.status = 404;
+        throw error;
+    }
+    const userAccusor = await userHelper.getUserById(request.body.id);
+    const userAccusorPassMatch = await bcrypt.compare(request.body.password, userAccusor.password);
+    if (!userAccusorPassMatch) {
+        const error = new Error(`The password you've entered is incorrect. Try again`);
+        error.status = 401;
+        throw error;
+    }
+    await userHelper.delete(userTargetId);
+    response.status(200).json({ message: `User Account with ${userTargetId} ID has been deleted` });
+});
--- api/helpers/user.helper.js
+++ api/helpers/user.helper.js
@@ -1,5 +1,6 @@
 const User = require('../models/user');
 
-exports.getUserById = (userId) => {
-    return User.findById(userId);
-}
+exports.getUserById = (userId) => User.findById(userId);
+exports.getUserByUsernameOrEmail = (usernameOrEmail) => User.findOne({ $or: [{ username: usernameOrEmail }, { email: usernameOrEmail }] })
+exports.create = (UserObject) => UserObject.save();
+exports.delete = (userId) => User.deleteOne({ _id: userId });
